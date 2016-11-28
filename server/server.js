edb39d31e1a2de0cfb502cddb471b3ae3ba1681a
XianAquino <xian_a@yahoo.com>
2016-11-27 19:26:24 -0600

    Started cliet side

--- server/server.js
+++ server/server.js
@@ -6,17 +6,25 @@ var port = 3000;
 
 var server = http.createServer(function(req,res){
 
-  if(req.method === 'GET' && req.url === '/secretWord'){
-
-    fs.readFile(__dirname + '/data/dictionary.txt','utf-8', function(err,data){
-      if(err){
-        console.log("error:",err);
-      }
-      var secretWord = pickWord(data.split('\n'));
-      console.log(secretWord);
-    })
+  if(req.method === 'GET'){
+    if(req.url === '/') {
+      fs.readFile(__dirname + '/../client/index.html', function(err,data){
+        res.writeHead(200, "Content-Type:application/html");
+        res.end(data);
+      })
+    }
+    else if(req.url === '/secretWord') {
+      fs.readFile(__dirname + '/data/dictionary.txt','utf-8', function(err,data){
+        if(err){
+          console.log("error:",err);
+        }else{
+          var secretWord = pickWord(data.split('\n'));
+          res.writeHead(200, "Content-Type:application/json");
+          res.end(JSON.stringify(secretWord));
+        }
+      })
+    }
   }
-
 });
 console.log(`Listening on port: ${port}`);
 
